e35b566daf4aacd1170939e3f433b6c027a69ec7
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-10-10 21:32:52 +0300

    docs: add docstring to Json methods

--- src/py_json/json.py
+++ src/py_json/json.py
@@ -9,14 +9,18 @@ if TYPE_CHECKING:
 class Json:
     @staticmethod
     def load(fp: TextIO) -> JsonData:
+        """Load json from a file."""
         raise NotImplementedError
 
     @staticmethod
     def loads(s: str) -> JsonData:
+        """Load json from a string."""
         raise NotImplementedError
 
     def dump(obj: JsonData, fp: TextIO) -> None:
+        """Serialize into json and write to file."""
         raise NotImplementedError
 
     def dumps(obj: JsonData) -> str:
+        """Serialize into json and return as a string."""
         raise NotImplementedError
